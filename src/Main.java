3db4d71de44a723162f954295dd33b2390f241cb
Cornerstone Ephraim <fortunecornerstone@gmail.com>
2023-02-08 18:31:49 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        // code goes here
-        System.out.println("Cornerstone");
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
     }
 }
