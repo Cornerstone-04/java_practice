2647e41e64fa33a4ead1a93eafe9ad70dd3518d1
Cornerstone Ephraim <fortunecornerstone@gmail.com>
2023-02-12 17:36:00 +0100

    feat: modify ternary

--- src/App.java
+++ src/App.java
@@ -1,8 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // System.out.println("Hello, World!");
-        var number = (Math.random() < 0) ? "negative" : "positive";
-        System.out.print("The number is " + number);
+
+        /* ternary operator example */
+        var number = (Math.random() > 0) ? "negative" : "positive";
+        System.out.print("The number is " + number + ".");
 
     }
 }
