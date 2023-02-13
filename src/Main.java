dfe3491a28941e43ccc290e559e1494246d9d4df
Cornerstone Ephraim <fortunecornerstone@gmail.com>
2023-02-13 08:48:54 +0100

    feat: Add voting file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,42 @@
+import java.util.Date;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        /**
+         * primitive types
+         * There are 8 primitive types:
+         * byt, short, long, int, float, double, char, boolean.
+         */
         byte age = 30;
+        short pump = 345;
         long viewsCount = 3_123_456_789L;
+        int x = 50;
+        float price = 10.99F;
+        double y = 5.566D;
+        char letter = 'A';
+        boolean isEligible = false;
+
+        /**
+         * Reference types
+         * 
+         */
+        byte old = 30;
+        Date now = new Date();
+        System.out.println(now);
+
+        byte a = 1, b = a;
+        a = 2;
+        System.out.println(b);
+
+        Point point = new Point(1, 1);
+        Point point2 = point;
+        point.x = 4;
+        System.out.println(point2);
+
+        String message = "Hello World" + "!!";
+        System.out.println(message.endsWith("!!"));
+        System.out.println(message);
     }
 }
---
+++ src/Voting.java
@@ -0,0 +1,8 @@
+public class Voting {
+    public static void main(String[] args) {
+        int myAge = 30;
+        int votingAge = 18;
+        String res = (myAge > votingAge) ? "Eligible to vote" : "Too young to vote";
+        System.out.println(res);
+    }
+}
